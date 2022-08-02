03477c1f322647052f105ebe549162b914904928
EC2 Default User <ec2-user@ip-172-31-21-116.ca-central-1.compute.internal>
2022-08-02 03:29:07 +0000

    project done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,27 @@
 class ApplicationController < ActionController::Base
-  #
+  #protect_form_forgery with: :exception
+  
+before_action :require_user
+  
+  helper_method :current_user, :logged_in?
+
+#we can use these methods all over the project
+
+  def current_user
+      @current_user ||= Student.find(session[:student_id]) if session[:student_id]
+      #memorization method  
+  end
+  
+  def logged_in?
+      !!current_user #boolean method
+      
+  end
+  
+  def require_user
+  
+    if !logged_in?
+      flash[:notice] = "You must be logged  in to perform this action"
+      redirect_to login_path
+    end
+  end
 end
